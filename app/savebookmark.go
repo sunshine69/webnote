1f08b8f96bbc68137a0693ca62c8c4f9f6d6ffb4
Steve Kieu <steve.kieu@ladbrokes.com.au>
2022-07-03 12:17:25 +1000

    Add some doco about how to use the bookmark app

--- app/savebookmark.go
+++ app/savebookmark.go
@@ -7,6 +7,8 @@ import (
 
 	m "github.com/sunshine69/webnote-go/models"
 )
+// Create a note with title `Bookmarks` and with the content is the file bookmark-note.html in the same folder.
+// remember to replace the edit link with the note ID - it is like a normal note so you can see it int he url when you edit the note.
 
 func SaveBookMark(w http.ResponseWriter, r *http.Request) {
 	bmarkdNote := m.GetNote("Bookmarks")
@@ -14,6 +16,7 @@ func SaveBookMark(w http.ResponseWriter, r *http.Request) {
 	mytitle := m.GetRequestValue(r, "title", "")
 	is_ajax := m.GetRequestValue(r, "is_ajax", "0")
 	if myurl != "" {
+		//The marker text is the pattern
 		ptn := regexp.MustCompile(`(\<li\>[=]+ Form [=]+\<\/li\>)`)
 		newText := fmt.Sprintf("<li><a href=\"%s\" title=\"%s\">%s</a></li><a href=\"/delbookmark?url=%s\">remove</a>\n<br/>$1", myurl, mytitle,myurl, myurl)
 		newCt := ptn.ReplaceAllString(bmarkdNote.Content, newText)
